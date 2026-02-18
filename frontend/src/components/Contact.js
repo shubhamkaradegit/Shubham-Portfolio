794bfe5649677b81799ef1e31f1b6aa4043821f4
Shubham Karade <infoshubham1196@gmail.com>
2026-02-18 21:20:49 +0530

    fetch contac

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -59,7 +59,7 @@ const Contact = () => {
     try {
       // ✅ 1️⃣ Save to Backend (optional)
       try {
-        await fetch(`${process.env.REACT_APP_API_URL}/contact`, {
+        await fetch(`${process.env.REACT_APP_API_URL}/contacts`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
